42575eeb1ced45617bac8ec52ca26b996155f269
Jakub Gladykowski <gladykov@gmail.com>
2025-10-26 20:33:06 -0300

    Dist again

--- dist/hello.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@actions/core");
-console.log('hello world');
-console.log((0, core_1.getInput)('clue'));
