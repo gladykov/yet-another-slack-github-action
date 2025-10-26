5a916fd8753155f7e0d93eabd86c2f6a5cefe8fb
Jakub Gladykowski <gladykov@gmail.com>
2025-10-26 20:16:17 -0300

    Compile this stuff

--- dist/hello.js
+++ dist/hello.js
@@ -1,2 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@actions/core");
 console.log('hello world');
-console.log(process.env['clue']);
+console.log((0, core_1.getInput)('clue'));
